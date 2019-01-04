64b3b23cd17be43ac70723a72a91113f9356c535
Louis Klein <louis.klein@aboutgoods.net>
2019-01-04 18:09:38 +0100

    feat: using pandas for efficiency

--- prepare_data.py
+++ prepare_data.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
+import pandas
+import numpy as np
 
-dataset_name = 'own'
-sentences = ['Would you like a plain sweater or something else?​', 'Great. We have some very nice wool slacks over here. Would you like to take a look?']
-labels = ['Yes' , 'No' ]
-train_or_test_list = ['train', 'test']
+def _main():
+    dataset_name = 'rr'
 
+    with open("./data/{}/train.csv".format(dataset_name)) as train_file:
+        df_train = pandas.read_csv(train_file, names=["sentences", "label"])
+        df_train = df_train.assign(train='train')
 
-meta_data_list = []
+    with open("./data/{}/test.csv".format(dataset_name)) as train_file:
+        df_test = pandas.read_csv(train_file, names=["sentences", "label"])
+        df_test = df_test.assign(train='test')
 
-for i in range(len(sentences)):
-    meta = str(i) + '\t' + train_or_test_list[i] + '\t' + labels[i]
-    meta_data_list.append(meta)
+    df = df_train.append(df_test, ignore_index=True)
 
-meta_data_str = '\n'.join(meta_data_list)
+    with open("./data/corpus/{}.txt".format(dataset_name), 'w') as corpus_file:
+        np.savetxt(corpus_file, df.values[:,0], fmt='%s')
 
-f = open('data/' + dataset_name + '.txt', 'w')
-f.write(meta_data_str)
-f.close()
+    with open("./data/{}.txt".format(dataset_name), 'w') as metadata_file:
+        df.to_csv(metadata_file, header=None, columns=["train", "label"], sep='\t', mode='a') 
 
-corpus_str = '\n'.join(sentences)
-
-f = open('data/corpus/' + dataset_name + '.txt', 'w')
-f.write(corpus_str)
-f.close()
+if __name__ == "__main__":
+    _main()
